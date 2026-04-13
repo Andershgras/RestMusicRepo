fb8f74eb65bdd9485fdef70ea15772bc5f810f78
bilalsarwar2907 <bilalsarwar2907@gmail.com>
2026-04-13 10:11:40 +0200

    ModelClass is Done

---
+++ RestMusic.Domain/Models/MusicRecord.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestMusic.Domain.Models
+{
+    public class MusicRecord
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }= string.Empty;
+        public string Artist { get; set; }= string.Empty;
+        public int DurationInSeconds { get; set; } = 0;
+
+        public int publicationYear { get; set; } = 0;
+
+        public override string ToString()
+        {
+            return $"Id: {Id}, Title: {Title}, Artist: {Artist}, Duration: {DurationInSeconds} seconds, Publication Year: {publicationYear}";
+        }
+    }
+}
