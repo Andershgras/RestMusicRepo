b5088873b2b6b8042e64b1df590e9e8e40f53cb6
Andershgras <andershgras@gmail.com>
2026-04-14 11:11:00 +0200

    Added Swagger

--- RestMusic.Api/Controllers/AuthController.cs
+++ RestMusic.Api/Controllers/AuthController.cs
@@ -9,7 +9,7 @@ using System.Text;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
-namespace Club100API.Controllers
+namespace RestMusic.Api.Controllers
 {
 
     [Route("api/[controller]")]
