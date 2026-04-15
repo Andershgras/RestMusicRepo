4dcc9535f1c51394b431aedfd61699803189049c
bilalsarwar2907 <bilalsarwar2907@gmail.com>
2026-04-15 13:05:18 +0200

    Succesfully Added Delte And Tested

--- RestMusic.Api/Controllers/RecordsController.cs
+++ RestMusic.Api/Controllers/RecordsController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using RestMusic.Domain.Repositories;
+using RestMusic.Domain.Models;
 
 namespace RestMusic.Api.Controllers
 {
@@ -33,6 +34,38 @@ namespace RestMusic.Api.Controllers
             }
 
             return Ok(records);
+        }
+        [HttpGet("{id}")]
+        public ActionResult GetById(int id)
+        {
+            var record = _repo.GetAll().FirstOrDefault(r => r.Id == id);
+            if (record == null)
+            {
+                return NotFound();
+            }
+            return Ok(record);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        // [Authorize(Roles = "Admin")]
+        public ActionResult<MusicRecord> Post([FromBody] MusicRecord newRecord)
+        {
+            MusicRecord theRecord = _repo.Add(newRecord);
+            string uri = Url.RouteUrl(RouteData.Values) + "/" + theRecord.Id;
+            return Created(uri, theRecord);
+        }
+        // DELETE api/<CatsController>/5
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        //[Authorize(Roles = "Admin")]
+        public ActionResult<MusicRecord> Delete(int id)
+        {
+            MusicRecord? removedRecord = _repo.Delete(id);
+            if (removedRecord == null) 
+            { return NotFound("No such record, id: " + id); }
+            return Ok(removedRecord);
         }
     }
 }
