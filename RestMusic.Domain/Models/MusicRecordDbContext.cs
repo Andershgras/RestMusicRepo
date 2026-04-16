7e5f0361c61337d02c53e1db23624a18cabe412c
bilalsarwar2907 <bilalsarwar2907@gmail.com>
2026-04-16 10:18:08 +0200

    DbContext is Created

---
+++ RestMusic.Domain/Models/MusicRecordDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestMusic.Domain.Models
+{
+    public class MusicRecordDbContext : DbContext
+    {
+        private readonly MusicRecordDbContext _context;
+
+        public MusicRecordDbContext(DbContextOptions<MusicRecordDbContext> options) : base(options)
+        {
+        }
+        public DbSet<MusicRecord> Artists { get; set; } = null!;
+    }
+}
